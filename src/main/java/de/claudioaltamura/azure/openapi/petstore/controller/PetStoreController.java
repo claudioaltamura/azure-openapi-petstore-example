e6162d6a7bafbbbeb8c0db443877d26e8ea3de57
claudioaltamura <claudio@claudioaltamura.de>
2024-09-06 18:43:08 +0200

    feat: added #1 version add and find

--- src/main/java/de/claudioaltamura/azure/openapi/petstore/controller/PetStoreController.java
+++ src/main/java/de/claudioaltamura/azure/openapi/petstore/controller/PetStoreController.java
@@ -1,6 +1,10 @@
 package de.claudioaltamura.azure.openapi.petstore.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,17 @@ import jakarta.validation.Valid;
 @RestController
 public class PetStoreController implements DefaultApi {
 
+    private final Map<String, List<Pet>> pets = new HashMap<>();
+
+    private final AtomicLong id = new AtomicLong(1);
+
     @Override
     public ResponseEntity<Pet> addPet(@Valid NewPet newPet) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addPet'");
+        var tag = newPet.getTag();
+        pets.putIfAbsent(tag, new ArrayList<Pet>());
+        var petToAdd = Pet.builder().id(id.getAndIncrement()).name(newPet.getName()).tag(tag).build();
+        pets.get(tag).add(petToAdd);
+        return ResponseEntity.ok(petToAdd);
     }
 
     @Override
@@ -33,8 +44,13 @@ public class PetStoreController implements DefaultApi {
 
     @Override
     public ResponseEntity<List<Pet>> findPets(@Valid List<String> tags, @Valid Integer limit) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findPets'");
+        var allPets = new ArrayList<Pet>();
+        tags.forEach(tag -> {
+            if(pets.containsKey(tag)) {
+                allPets.addAll(pets.get(tag));
+            }
+        });
+        return ResponseEntity.ok(allPets);
     }
     
 }
