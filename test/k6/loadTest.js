d118cb4db369a3108d6c075299561133a3cd5685
claudioaltamura <claudio@claudioaltamura.de>
2024-09-18 09:15:38 +0200

    test: added config

--- test/k6/loadTest.js
+++ test/k6/loadTest.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { BASEURL } from './config.js';
 
 export const options = {
   vus: 5,
@@ -7,6 +8,6 @@ export const options = {
 };
 
 export default function () {
-  http.get('http://test.k6.io');
+  http.get(BASEURL);
   sleep(1);
 }
